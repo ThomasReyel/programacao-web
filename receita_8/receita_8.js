eedb2c0bae67a1874af76bae6d35474ee6107953
ThomasReyel <thomasreyel2231@gmail.com>
2026-09-21 21:03:46 -0300

    add

---
+++ receita_8/receita_8.js
@@ -0,0 +1,12 @@
+function CarregarTabela(
+    cs,
+    id,
+    propriedades,
+) {
+    const cabecalho = propriedades.map((p) => `<th>${p}</th>`).join("");
+    const itensHtml = cs
+        .map((item) => `<tr>${propriedades.map((p) => `<td>${item[p]}</td>`).join("")}</tr>`)
+        .join("");
+
+    document.getElementById(id).innerHTML = `<table><tr>${cabecalho}</tr>${itensHtml}</table>`;
+}
